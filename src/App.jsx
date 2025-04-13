21ee1802f9285f8ab6b12b2688354a620e5b2536
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2025-04-13 15:49:55 -0600

    deploy react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='text-center font-bold'>Parroquia Divino Salvador</h1>
+    </>
+  )
+}
+
+export default App
